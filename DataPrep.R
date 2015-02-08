c6945cb3aec77d4b3e850c817d5bb76b81e73e1e
Grant Oliveira <grant.oliveira>
2015-02-08 14:30:15 -0800

    Adding all the needed files

---
+++ DataPrep.R
@@ -0,0 +1,22 @@
+## Check if you got dat der file
+if (!file.exists("./household_power_consumption.txt")) {
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip")
+  unzip("./exdata-data-household_power_consumption.zip")
+}
+
+filename <- "./household_power_consumption.txt"
+file <- read.table(filename,
+                   header = TRUE,
+                   sep = ";",
+                   colClasses = c("character", "character", rep("numeric",7)),
+                   na = "?")
+dim(data) 
+attach(data)
+dates <- Date == "1/2/2007" | Date == "2/2/2007"
+newData <- file[dates, ]
+attach(newData)
+x <- paste(Date, Time)
+newData$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(newData) <- 1:nrow(newData)
+dim(newData)
+attach(newData)
---
+++ Plot 4.R
@@ -0,0 +1,32 @@
+source("DataPrep.R")
+png(filename = "plot4.png", 
+    width = 480, height = 480,
+    units = "px", bg = "transparent")
+par(mfrow = c(2, 2))
+## Top Left
+plot(DateTime, Global_active_power, 
+     type = "l",
+     xlab = "", ylab = "Global Active Power")
+## Top Right
+plot(DateTime, Voltage,
+     type = "l",
+     xlab = "datetime", ylab = "Voltage")
+## Bottom Left
+plot(DateTime, Sub_metering_1, 
+     type = "l",
+     col = "black",
+     xlab = "", ylab = "Energy sub metering")
+lines(DateTime, Sub_metering_2, col = "red")
+lines(DateTime, Sub_metering_3, col = "blue")
+## Bottom Right
+plot(DateTime, Global_reactive_power, 
+     type = "l",
+     col = "black",
+     xlab = "datetime", ylab = colnames(newData)[4])
+legend("topright", 
+       bty = "n",
+       col = c("black", "red", "blue"),
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lwd = 1)
+
+dev.off()
